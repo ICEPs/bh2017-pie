ac7dae67f54d69e82cb2604d23631bdb6071352b
Ian De La Cruz <ridlc10@gmail.com>
2017-02-19 05:15:51 +0800

    Updated schema'

--- Web/simot-api/db/schema.rb
+++ Web/simot-api/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170218170629) do
+ActiveRecord::Schema.define(version: 20170218211011) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,8 @@ ActiveRecord::Schema.define(version: 20170218170629) do
     t.integer  "urgency"
     t.datetime "created_at",       null: false
     t.datetime "updated_at",       null: false
+    t.datetime "expiry_date"
+    t.string   "donation_title"
   end
 
   create_table "transactions", force: :cascade do |t|
