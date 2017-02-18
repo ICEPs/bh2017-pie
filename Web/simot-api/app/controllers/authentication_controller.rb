8078ba46bb9dc30bbed33b58dcc8a6bbcfacc5bf
Ian De La Cruz <ridlc10@gmail.com>
2017-02-19 06:46:10 +0800

    Add user id to return value in authentication

--- Web/simot-api/app/controllers/authentication_controller.rb
+++ Web/simot-api/app/controllers/authentication_controller.rb
@@ -5,7 +5,8 @@ class AuthenticationController < ActionController::API
     command = AuthenticateUser.call params[:email], params[:password]
 
     if command.success?
-      render json: {auth_token: command.result}
+      render json: {auth_token: command.result,
+                    user_id: JsonWebToken.decode(command.result)['user_id']}
     else
       render json: {error: command.errors}, status: :unauthorized
     end
