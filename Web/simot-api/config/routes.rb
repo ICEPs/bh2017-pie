a5b3e911e0f492d63490e67000f427c24b3f699b
Ian De La Cruz <ridlc10@gmail.com>
2017-02-19 04:40:55 +0800

    Add auth route

--- Web/simot-api/config/routes.rb
+++ Web/simot-api/config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
   resources :users
+  resources :items
+  resources :donations
+  post 'authenticate', to: 'authentication#authenticate'
+
 end
