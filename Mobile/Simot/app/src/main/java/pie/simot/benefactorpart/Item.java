203eda9040e3feb3cbabc2be30773ebee54431e5
ejcris <elysiajelenavilladarez@yahoo.com>
2017-02-19 06:53:54 +0800

    Added add layout

--- Mobile/Simot/app/src/main/java/pie/simot/benefactorpart/Item.java
+++ Mobile/Simot/app/src/main/java/pie/simot/benefactorpart/Item.java
@@ -1,8 +1,66 @@
 package pie.simot.benefactorpart;
 
+import java.util.Date;
+
 /**
  * Created by elysi on 2/18/2017.
  */
 
 public class Item {
+    private String items;
+    private String companyName;
+    private String itemDesc;
+    private Date expiryDate;
+
+    private int urgencyLevel;
+    private Date createdDate;
+
+    public String getItems() {
+        return items;
+    }
+
+    public void setItems(String items) {
+        this.items = items;
+    }
+
+    public String getCompanyName() {
+        return companyName;
+    }
+
+    public void setCompanyName(String companyName) {
+        this.companyName = companyName;
+    }
+
+    public String getItemDesc() {
+        return itemDesc;
+    }
+
+    public void setItemDesc(String itemDesc) {
+        this.itemDesc = itemDesc;
+    }
+
+    public Date getExpiryDate() {
+        return expiryDate;
+    }
+
+    public void setExpiryDate(Date expiryDate) {
+        this.expiryDate = expiryDate;
+    }
+
+    public Date getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(Date createdDate) {
+        this.createdDate = createdDate;
+    }
+
+    public int getUrgencyLevel() {
+
+        return urgencyLevel;
+    }
+
+    public void setUrgencyLevel(int urgencyLevel) {
+        this.urgencyLevel = urgencyLevel;
+    }
 }
--- Mobile/Simot/app/src/main/java/pie/simot/beneficiarypart/Call.java
+++ Mobile/Simot/app/src/main/java/pie/simot/beneficiarypart/Call.java
@@ -1,8 +1,55 @@
 package pie.simot.beneficiarypart;
 
+import java.util.Date;
+
 /**
  * Created by elysi on 2/18/2017.
  */
 
 public class Call {
+    private String postTitle;
+    private String messageBody;
+    private String orgName;
+    private int urgencyLevel;
+    private Date createdDate;
+
+    public String getPostTitle() {
+        return postTitle;
+    }
+
+    public void setPostTitle(String postTitle) {
+        this.postTitle = postTitle;
+    }
+
+    public String getMessageBody() {
+        return messageBody;
+    }
+
+    public void setMessageBody(String messageBody) {
+        this.messageBody = messageBody;
+    }
+
+    public String getOrgName() {
+        return orgName;
+    }
+
+    public void setOrgName(String orgName) {
+        this.orgName = orgName;
+    }
+
+    public int getUrgencyLevel() {
+        return urgencyLevel;
+    }
+
+    public void setUrgencyLevel(int urgencyLevel) {
+        this.urgencyLevel = urgencyLevel;
+    }
+
+    public Date getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(Date createdDate) {
+        this.createdDate = createdDate;
+    }
 }
