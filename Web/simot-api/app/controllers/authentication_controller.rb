3cb7221b84d074db4b7ea59ac05307b3c9fb9ec3
Ian De La Cruz <ridlc10@gmail.com>
2017-02-19 08:57:31 +0800

    Make auth return user type

--- Web/simot-api/app/controllers/authentication_controller.rb
+++ Web/simot-api/app/controllers/authentication_controller.rb
@@ -4,9 +4,12 @@ class AuthenticationController < ActionController::API
   def authenticate
     command = AuthenticateUser.call params[:email], params[:password]
 
+    user = User.where(:id => JsonWebToken.decode(command.result)['user_id']).take
+
     if command.success?
-      render json: {auth_token: command.result,
-                    user_id: JsonWebToken.decode(command.result)['user_id']}
+      render json: { auth_token: command.result,
+                    user_id: user.id,
+                    user_type: user.user_type }
     else
       render json: {error: command.errors}, status: :unauthorized
     end
