dbb1664f9037fc5d98803f3d8d0ba1927c464f0d
Ian De La Cruz <ridlc10@gmail.com>
2017-02-19 08:58:01 +0800

    Updated Schema

--- Web/simot-api/db/schema.rb
+++ Web/simot-api/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170218234109) do
+ActiveRecord::Schema.define(version: 20170219004938) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -61,7 +61,7 @@ ActiveRecord::Schema.define(version: 20170218234109) do
     t.string   "rep_name"
     t.string   "rep_contact_info"
     t.string   "company_description"
-    t.string   "type"
+    t.string   "user_type"
     t.datetime "created_at",          null: false
     t.datetime "updated_at",          null: false
   end
